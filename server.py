b573321bbce363876582a39480b7c899a8e6e027
sankalp6115 <sankalpomar6115@gmail.com>
2026-01-11 21:10:24 +0530

    Resolving production issues

--- server.py
+++ server.py
@@ -1,4 +1,3 @@
-import os
 from flask import Flask, request, jsonify
 from flask_cors import CORS
 import numpy as np
@@ -26,5 +25,4 @@ def predict():
     })
 
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 5000))
-    app.run(host="0.0.0.0", port=port)
+    app.run(debug=True)
